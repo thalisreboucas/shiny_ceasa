994581a9beb6d916a37aa12a886c981ec14160bc
thalisreboucas <thalisreboucasdeliveira@gmail.com>
2023-06-05 18:12:26 -0300

    atualização

--- app.R
+++ app.R
@@ -180,7 +180,7 @@ actual_price_tab <- tabItem(
     tabBox(
       title = "Gráfico da distribuição de preços (Boxplot)", 
       closable = FALSE, 
-      width = 10,
+      width = 12,
       solidHeader = TRUE, 
       status = "primary",
       collapsible = TRUE,
@@ -234,153 +234,7 @@ actual_price_tab <- tabItem(
 forcast_model_tab <- tabItem(
   tabName = "forecast_tab",
   fluidRow(
-    userBox(
-      title = userDescription(
-        image = "https://adminlte.io/themes/AdminLTE/dist/img/user1-128x128.jpg",
-        title = "User card type 1",
-        subtitle = "a subtitle here"
-      ),
-      collapsible = FALSE,
-      ribbon(
-        text = "New user",
-        color = "fuchsia"
-      ),
-      status = "purple",
-      elevation = 4,
-      "Any content here"
-    ),
-    userBox(
-      title = userDescription(
-        type = 2,
-        image = "https://adminlte.io/themes/AdminLTE/dist/img/user7-128x128.jpg",
-        title = "User card type 2",
-        subtitle = "a subtitle here",
-        imageElevation = 4
-      ),
-      status = "teal",
-      background = "teal",
-      maximizable = TRUE,
-      gradient = TRUE,
-      progressBar(
-        value = 5,
-        striped = FALSE,
-        status = "info"
-      ),
-      progressBar(
-        value = 20,
-        striped = TRUE,
-        status = "warning"
-      )
-    )
-  ),
-  fluidRow(
-    socialBox(
-      title = userBlock(
-        image = "https://adminlte.io/themes/AdminLTE/dist/img/user4-128x128.jpg",
-        title = "Social Box",
-        subtitle = "example-01.05.2018"
-      ),
-      "Some text here!",
-      attachmentBlock(
-        image = "https://adminlte.io/themes/v3/dist/img/user1-128x128.jpg",
-        title = "Test",
-        href = "https://google.com",
-        "This is the content"
-      ),
-      lapply(X = 1:10, FUN = function(i) {
-        boxComment(
-          image = "https://adminlte.io/themes/AdminLTE/dist/img/user3-128x128.jpg",
-          title = paste("Comment", i),
-          date = "01.05.2018",
-          paste0("The ", i, "-th comment")
-        )
-      }),
-      footer = "The footer here!"
-    ),
-    box(
-      title = "Box with user comment",
-      status = "primary",
-      userPost(
-        id = 1,
-        image = "https://adminlte.io/themes/AdminLTE/dist/img/user1-128x128.jpg",
-        author = "Jonathan Burke Jr.",
-        description = "Shared publicly - 7:30 PM today",
-        "Lorem ipsum represents a long-held tradition for designers, 
-       typographers and the like. Some people hate it and argue for 
-       its demise, but others ignore the hate as they create awesome 
-       tools to help create filler text for everyone from bacon 
-       lovers to Charlie Sheen fans.",
-        userPostTagItems(
-          userPostTagItem(dashboardBadge("item 1", color = "warning")),
-          userPostTagItem(dashboardBadge("item 2", color = "danger"))
-        )
-      ),
-      userPost(
-        id = 2,
-        image = "https://adminlte.io/themes/AdminLTE/dist/img/user6-128x128.jpg",
-        author = "Adam Jones",
-        description = "Shared publicly - 5 days ago",
-        userPostMedia(image = "https://adminlte.io/themes/AdminLTE/dist/img/photo2.png"),
-        userPostTagItems(
-          userPostTagItem(dashboardBadge("item 1", color = "info")),
-          userPostTagItem(dashboardBadge("item 2", color = "danger"))
-        )
-      )
-    )
-  ),
-  fluidRow(
-    box(
-      status = "primary",
-      width = 3,
-      solidHeader = TRUE,
-      boxProfile(
-        image = "https://adminlte.io/themes/AdminLTE/dist/img/user4-128x128.jpg",
-        title = "Nina Mcintire",
-        subtitle = "Software Engineer",
-        bordered = TRUE,
-        boxProfileItem(
-          title = "Followers",
-          description = 1322
-        ),
-        boxProfileItem(
-          title = "Following",
-          description = 543
-        ),
-        boxProfileItem(
-          title = "Friends",
-          description = 13287
-        )
-      )
-    ),
-    box(
-      title = "Card with messages",
-      width = 9,
-      footer =  tagList(
-        actionButton("remove_message", "Remove"),
-        actionButton("add_message", "Add"),
-        actionButton("update_message", "Update"),
-        numericInput("index_message", "Message index:", 1, min = 1, max = 3)
-      ),
-      userMessages(
-        width = 6,
-        status = "danger",
-        id = "message",
-        userMessage(
-          author = "Alexander Pierce",
-          date = "20 Jan 2:00 pm",
-          image = "https://adminlte.io/themes/AdminLTE/dist/img/user1-128x128.jpg",
-          type = "received",
-          "Is this template really for free? That's unbelievable!"
-        ),
-        userMessage(
-          author = "Sarah Bullock",
-          date = "23 Jan 2:05 pm",
-          image = "https://adminlte.io/themes/AdminLTE/dist/img/user3-128x128.jpg",
-          type = "sent",
-          "You better believe it!"
-        )
-      )
-    )
+    
   )
 )
 
@@ -887,17 +741,18 @@ shinyApp(
                                        dplyr::group_by(id) %>%
                                        dplyr::filter(id == item()) %>% 
                                        timetk::tk_seasonal_diagnostics(.date_var = date,.value = value) %>% 
-                                       dplyr::filter(year == year_violin()) %>% 
+                                       dplyr::group_by(year) %>% 
+                                       dplyr::filter(year == year_violin()) %>%
                                        dplyr::summarize( Mínimo = min(.value),
-                                                                                          Média = mean(.value),`Desvio Padrão` = sd(.value) , 
-                                                                                          IQR = IQR(.value) , 
-                                                                                          Curtose = as.numeric(kurtosis(.value)) , 
-                                                                                          Assimetria = as.numeric(skewness(.value)),
-                                                                                          Q1 = stats::quantile(.value,probs = 0.25, na.rm = TRUE), 
-                                                                                          Mediana = median(.value),
-                                                                                          Q3 = stats::quantile(.value,probs = 0.75, na.rm = TRUE),
-                                                                                          Maxímo = max(.value),
-                                                                                          Amplitude = (max(.value) -min(.value))) 
+                                                         Média = mean(.value),`Desvio Padrão` = sd(.value) , 
+                                                         IQR = IQR(.value) , 
+                                                         Curtose = as.numeric(kurtosis(.value)) , 
+                                                         Assimetria = as.numeric(skewness(.value)),
+                                                         Q1 = stats::quantile(.value,probs = 0.25, na.rm = TRUE), 
+                                                         Mediana = median(.value),
+                                                         Q3 = stats::quantile(.value,probs = 0.75, na.rm = TRUE),
+                                                         Maxímo = max(.value),
+                                                         Amplitude = (max(.value) -min(.value))) 
                                      
     })
     
